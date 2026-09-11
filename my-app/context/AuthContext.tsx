95f61baaa7b72075536ee2e61c30dba513cb6a18
sadhna2003 <sadhna82003@gmail.com>
2026-09-11 09:27:59 +0000

    add context api example

--- my-app/context/AuthContext.tsx
+++ my-app/context/AuthContext.tsx
@@ -1,36 +1,40 @@
 "use client";
-import {createContext , useContext,useState} from "react";
+import { createContext, useContext, useState } from "react";
 import React from "react";
-
-export const AuthContext = createContext();
-
+type User = { name: string; email: string; }; 
+type AuthContextType = {
+   user: User | null; 
+   login: () => void; 
+   logout: () => void; 
+  };
+export const AuthContext = createContext<AuthContextType | undefined>( undefined );
 export const AuthProvider = ({
   children,
 }: {
   children: React.ReactNode;
-})=>{
-    const [user , setUser] = useState(null);
+}) => {
+  const [user, setUser] = useState<User | null>(null);
 
-    const login = ()=>{
-        setUser({
-            "name":"Test user",
-            "email":"test@test.com"
-        })
-    }
-    const logout = ()=>{
-        setUser(null)
-    }
-    return (
-       <AuthContext.Provider
-       value= {{
-         user,
-         login,
-         logout
-       }}
-       >
-        {children}
-        </AuthContext.Provider>
-    )
+  const login = () => {
+    setUser({
+      "name": "Test user",
+      "email": "test@test.com"
+    })
+  }
+  const logout = () => {
+    setUser(null)
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        login,
+        logout
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 export const useAuth = () => {
